0538ae24d0454dd4087d102301585286583e4ce0
Dan Seminara <japaneseforgaming@gmail.com>
2021-07-31 14:16:36 -0400

    Assignment Complete.

---
+++ index.js
@@ -0,0 +1,9 @@
+function add(a, b) { return a + b } //adds two numbers and returns the result
+function subtract(a, b) { return a - b } //subtracts b from a and returns the result
+function multiply(a, b) { return a * b } //multiplies two numbers and returns the result
+function divide(a, b) { return a / b } //divides a by b and returns the result
+function increment(n) { return n + 1 } //increments n and returns the result
+function decrement(n) { return n - 1 } //decrements n and returns the result
+
+function makeInt(n) { return parseInt(n, 10) }
+function preserveDecimal(n) { return parseFloat(n)}
